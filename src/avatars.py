13207888e3964f50b03ee9e0ae1e84ac87f64c7c
cefqrn <cefqrn@gmail.com>
2025-01-01 08:02:46 -0500

    added shared post support to avatars

--- src/avatars.py
+++ src/avatars.py
@@ -15,6 +15,10 @@ def get_filename(url):
     return url[i:]
 
 
+def handle_post(post):
+    avatars.add(post["postingProject"]["avatarURL"])
+
+
 cookie = Path(".cookie").read_text().strip()
 
 with INPUT_FILE.open() as f:
@@ -22,8 +26,11 @@ with INPUT_FILE.open() as f:
 
 avatars = set()
 for post in posts:
-    avatars.add(post["postingProject"]["avatarURL"])
+    handle_post(post)
+    for shared_post in post["shareTree"]:
+        handle_post(shared_post)
 
+written = 0
 for avatar in sorted(avatars):
     path = PATH / get_filename(avatar)
     if path.is_file():
@@ -33,5 +40,6 @@ for avatar in sorted(avatars):
     assert data_b is not None
 
     path.write_bytes(data_b)
+    written += 1
 
-print("done")
+print((written > 0)*'\n' + "done")
