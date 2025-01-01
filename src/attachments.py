9252f865ee245e5aa3a17317d8dcef105ac8ee0a
cefqrn <cefqrn@gmail.com>
2025-01-01 04:24:21 -0500

    added support for more block types

--- src/attachments.py
+++ src/attachments.py
@@ -3,7 +3,8 @@ from network import try_download
 from itertools import count
 from json import JSONDecodeError, load, loads, dump, dumps
 from pathlib import Path
-from re import search, I
+from re import I, search
+import re  # compile
 
 from sys import argv
 INPUT_FILE = Path(argv[1])
@@ -11,9 +12,16 @@ INPUT_FILE = Path(argv[1])
 PATH = Path("attachment")
 PATH.mkdir(parents=True, exist_ok=True)
 
+from string import ascii_letters, digits
+
+UUID_PATTERN = r"[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}"
+PATH_CHAR = fr"[-._~%!$&'()*+,;=:@{ascii_letters}{digits}]"
+
+CDN_URL = re.compile(fr"staging.cohostcdn.org/attachment/{UUID_PATTERN}/{PATH_CHAR}+\.[a-z]+", I)
+
 
 def get_filename(url):
-    m = search(r"[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}", url, I)
+    m = search(UUID_PATTERN, url, I)
     return m[0]
 
 
@@ -25,19 +33,33 @@ with INPUT_FILE.open() as f:
 attachments = set()
 for post in posts:
     for block in post["blocks"]:
-        if block["type"] != "attachment":
-            continue
-        
-        attachments.add(block["attachment"]["fileURL"])
+        match t := block["type"]:
+            case "attachment":
+                attachments.add(block["attachment"]["fileURL"])
+            case "attachment-row":
+                for attachment_block in block["attachments"]:
+                    assert attachment_block["type"] == "attachment"
+                    attachments.add(attachment_block["attachment"]["fileURL"])
+            case "markdown":
+                for url in CDN_URL.findall(block["markdown"]["content"]):
+                    attachments.add("https://" + url)
+            case "ask":
+                for url in CDN_URL.findall(block["ask"]["content"]):
+                    attachments.add("https://" + url)
+            case _:
+                raise ValueError(f"unknown block type: {t}")
 
+written = 0
 for attachment in sorted(attachments):
     path = PATH / get_filename(attachment)
     if path.is_file():
         continue
 
     data_b = try_download(cookie, attachment)
-    assert data_b is not None
+    if not data_b:
+        continue
 
     path.write_bytes(data_b)
+    written += 1
 
-print("done")
+print((written > 0)*'\n' + "done")
